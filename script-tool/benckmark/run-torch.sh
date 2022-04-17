45eeea46b9f329fefdcc9867c3c31a75c4d5f740
yixin <zxcxzcz@qq.com>
2022-04-17 10:36:25 +0800

    update
    Change-Id: Ifca19b5a325c6f8a7e1751d160abc2297a61585d

--- script-tool/benckmark/run-torch.sh
+++ script-tool/benckmark/run-torch.sh
@@ -1,7 +1,7 @@
 cd /ssd2/liuyixin04/workspace/my-parl/PARL/benchmark/torch/maddpg
 continous="continous"
 continous_add="--continuous_actions"
-experiment_name="benchmark-torch-continous"
+experiment_name="benchmark-torch-continous-extra"
 logdir="/ssd2/liuyixin04/workspace/logs/$experiment_name/"
 py="/ssd2/liuyixin04/miniconda3/envs/py37_torch/bin/python"
 pyfile="train.py"
@@ -21,8 +21,9 @@ seed=$RANDOM
 for act_spc in continous 
 do
 lib="/opt/compiler/gcc-8.2/lib/ld-linux-x86-64.so.2 --library-path /opt/compiler/gcc-8.2/lib:/usr/lib64:/ssd2/liuyixin04/miniconda3/envs/py37_paddle/lib:/home/work/cuda-10.0/lib64/:/home/work/cudnn_v7.4/cuda/lib64/:/ssd2/liuyixin04/.jumbo/lib"
-for env in simple simple_adversary simple_crypto simple_push simple_speaker_listener simple_spread simple_tag simple_world_comm
+# for env in simple simple_adversary simple_crypto simple_push simple_speaker_listener simple_spread simple_tag simple_world_comm
 # for env in simple_spread
+for env in simple_spread simple_tag simple_world_comm
 do
 command="$py $pyfile --env $env --model_dir $modeldir/s$seed-$env-$act_spc --seed $seed --log_dir $trainlog"
 if [ "$act_spc" = "$continous" ]
