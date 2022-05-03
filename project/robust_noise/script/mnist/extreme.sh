8ab0fc81d1723a57fa9d2c630602c037a7dae9e9
liuyixin04 <liuyixin04@baidu.com>
2022-05-03 12:24:02 +0800

    update
    Change-Id: Ida68b4985f37c9b77351363f1c879bbe8b8f242b

---
+++ project/robust_noise/script/mnist/extreme.sh
@@ -0,0 +1,29 @@
+root="/mnt/yixin/liuyixin-lib/project/robust_noise"
+cd $root
+source activate py38
+export CUDA_VISIBLE_DEVICES=0
+python generate_rem.py \
+    --arch resnet18 \
+    --dataset mnist-extreme \
+    --train-steps 422 \
+    --batch-size 128 \
+    --optim sgd \
+    --lr 0.1 \
+    --lr-decay-rate 0.1 \
+    --lr-decay-freq 211 \
+    --weight-decay 5e-4 \
+    --momentum 0.9 \
+    --pgd-radius 8 \
+    --pgd-steps 10 \
+    --pgd-step-size 1.6 \
+    --pgd-random-start \
+    --atk-pgd-radius 4 \
+    --atk-pgd-steps 10 \
+    --atk-pgd-step-size 0.8 \
+    --atk-pgd-random-start \
+    --samp-num 5 \
+    --report-freq 1000 \
+    --save-freq 1000 \
+    --data-dir ./data \
+    --save-dir ./exp_data/mnist-extreme/noise-test/rem8-4 \
+    --save-name rem
