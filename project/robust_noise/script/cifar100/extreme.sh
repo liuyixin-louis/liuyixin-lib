b5aec8b623cfb25a8e9ad99795bc3dc7619f93b0
liuyixin04 <liuyixin04@baidu.com>
2022-05-03 22:08:09 +0800

    f
    Change-Id: Ibdcec764959eca991bcd9fad70c0b9fdf27e4a58

--- project/robust_noise/script/cifar100/extreme.sh
+++ project/robust_noise/script/cifar100/extreme.sh
@@ -1,24 +1,65 @@
 cd $(dirname $(dirname $(dirname "$0")))
 source activate py38
 export CUDA_VISIBLE_DEVICES=0
-python train.py \
+train_epoch=15
+training_sample=300
+batch_size=128
+exp_name="cifar100-extreme"
+noise_name="baseline"
+attack_r=4
+protect_r=8
+
+noise_train_step=$(((train_epoch*training_sample)/batch_size))
+down_lr_step=$((train_step/2))
+report_step=$((train_step/2))
+train_eval_step=$((noise_train_step*2))
+train_eval_lr_decay_step=$((train_eval_step/2))
+report_step_eval=$((train_eval_step/2))
+
+python generate_rem.py \
     --arch resnet18 \
-    --dataset cifar100-extreme \
-    --train-steps 800 \
-    --batch-size 128 \
+    --dataset $exp_name \
+    --train-steps $noise_train_step \
+    --batch-size $batch_size \
     --optim sgd \
     --lr 0.1 \
     --lr-decay-rate 0.1 \
-    --lr-decay-freq 400 \
+    --lr-decay-freq $down_lr_step \
     --weight-decay 5e-4 \
     --momentum 0.9 \
-    --pgd-radius 4 \
+    --pgd-radius $protect_r \
+    --pgd-steps 10 \
+    --pgd-step-size 1.6 \
+    --pgd-random-start \
+    --atk-pgd-radius $attack_r \
+    --atk-pgd-steps 10 \
+    --atk-pgd-step-size 0.8 \
+    --atk-pgd-random-start \
+    --samp-num 5 \
+    --report-freq $report_step \
+    --save-freq 100000 \
+    --data-dir ./data \
+    --save-dir ./exp_data/$exp_name/$noise_name-noise/$protect_r-$attack_r \
+    --save-name $noise_name
+
+python train.py \
+    --arch resnet18 \
+    --dataset $exp_name \
+    --train-steps $train_eval_step \
+    --batch-size $batch_size \
+    --optim sgd \
+    --lr 0.1 \
+    --lr-decay-rate 0.1 \
+    --lr-decay-freq $train_eval_lr_decay_step \
+    --weight-decay 5e-4 \
+    --momentum 0.9 \
+    --pgd-radius $attack_r \
     --pgd-steps 10 \
     --pgd-step-size 0.8 \
     --pgd-random-start \
-    --report-freq 100 \
+    --report-freq $report_step_eval \
     --save-freq 100000 \
-    --noise-path ./exp_data/mnist-extreme/noise-test/rem8-4/rem-fin-def-noise.pkl \
+    --noise-path ./exp_data/$exp_name/$noise_name-noise/$protect_r-$attack_r/$noise_name-fin-def-noise.pkl \
     --data-dir ./data \
-    --save-dir ./exp_data/train/mnist-extreme/rem8-4/r4 \
+    --save-dir ./exp_data/train/$exp_name/rem$protect_r-$attack_r/ \
     --save-name train
